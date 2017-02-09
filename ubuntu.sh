3c176033c1ce5822a316a724eb8728d80a20c982
frankfang <frankfang1990@gmail.com>
2017-02-10 02:14:43 +0800

    add

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,18 +2,21 @@
 #sudo apt-get install software-properties-common -y
 #sudo apt-add-repository ppa:git-core/ppa -y
 #sudo add-apt-repository ppa:pkg-vim/vim-daily
-sudo sed -i 's/ppa.launchpad.net/launchpad.proxy.ustclug.org/g' /etc/apt/sources.list.d/git-core-ppa-trusty.list
-sudo apt-get update
+#sudo sed -i 's/ppa.launchpad.net/launchpad.proxy.ustclug.org/g' /etc/apt/sources.list.d/git-core-ppa-trusty.list
+#sudo apt-get update
 #sudo apt-get install git -y
-sudo apt-get upgrade vim -y
+#sudo apt-get install vim -y
 #sudo apt-get autoremove -y
+sudo apt-get install nginx -y
 
-if ! [ -f ~/.vim/autoload/plug.vim ]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
-fi 
-[ -f ~/.vimrc ] &&  mv ~/.vimrc ~/.vimrc.old 
-ln -s $(realpath ./vimrc) ~/.vimrc && echo "~/.vimrc copied"
-vim -c ":PlugInstall"
+
+## vim
+#if ! [ -f ~/.vim/autoload/plug.vim ]; then
+  #curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+#fi 
+#[ -f ~/.vimrc ] &&  mv ~/.vimrc ~/.vimrc.old 
+#ln -s "$(pwd -P)/vimrc" ~/.vimrc && echo "~/.vimrc copied"
+#vim -c ":PlugInstall"
 
 
 
